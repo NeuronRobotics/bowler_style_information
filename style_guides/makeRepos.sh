31de88e5c8c553d83a4ad7b42f9d1bee1e543b01
madhephaestus <mad.hephaestus@gmail.com>
2014-01-16 13:16:31 -0500

    Fixed the makeRepos script

--- style_guides/makeRepos.sh
+++ style_guides/makeRepos.sh
@@ -2,25 +2,44 @@
 
 function runClonePrune(){
 	temp=$1.tmp
-	#git clone bowler-monolithic $1.tmp
-	cd $temp
-	echo Updating git in: $PWD
+	if [ -d "$temp" ]; then
+		echo $temp Clone exists;
+	else
+		git clone bowler-monolithic $1.tmp;
+	fi
+	
+	cd $temp;
+	echo Updating git in: $PWD;
 	gitstring='git rm -r --cached --ignore-unmatch '$2
 	execString='git filter-branch -f --prune-empty --index-filter "'$gitstring'" HEAD'
 
 	echo $execString 
-	$execString
-return 
-	git remote remove origin
-	git remote add https://github.com/NeuronRobotics/$1.git
-	cd ..
-	git clone $temp $1
-	cd $1 
-	git remote remove origin
-	git remote add https://github.com/NeuronRobotics/$1.git
-	git push -f
+	eval $execString;
+	git remote remove origin;
+	git remote add https://github.com/NeuronRobotics/$1.git;
+	cd ..;
+	if [ -d "$1" ]; then
+		echo $1 Clone exists;
+	else
+		git clone $temp $1;
+	fi
+	
+	cd $1 ;
+	git remote remove origin;
+	git remote add https://github.com/NeuronRobotics/$1.git;
+	git push -f;
 }
 
-# "application/* csdk/* firmware/* hardware/* libs/* pcb/* plugin/* publish/* javasdk/* test/* Android/*"
+#runClonePrune  ''  'application/ csdk/ firmware/ hardware/ libs/ pcb/ plugin/ publish/ javasdk/ test/ Android/';
 
-runClonePrune  'hardware-bowler' 'application/ csdk/ firmware/ libs/ pcb/ plugin/ publish/ javasdk/ test/ Android/' ;
+runClonePrune  'hardware-bowler' 'application/ csdk/ firmware/ libs/ plugin/ publish/ javasdk/ test/ Android/' ;
+
+#runClonePrune  'microcontroller-bowler'  'application/ csdk/ firmware/tools/ firmware/library/ hardware/ libs/ pcb/ plugin/ publish/ javasdk/ test/ Android/';
+
+
+
+
+
+
+
+
